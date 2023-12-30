3353553445dd30185283832fb94ac2b85c86f188
Kriti Srivastava <76968430+kriti004@users.noreply.github.com>
2023-12-30 22:21:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const socket = new WebSocket('ws://localhost:3000');
-
-//     socket.addEventListener('message', (event) => {
-//         const chatOutput = document.getElementById('chat-output');
-//         chatOutput.innerHTML += `<p>${event.data}</p>`;
-//     });
-
-//     socket.addEventListener('open', (event) => {
-//         console.log('WebSocket connection opened:', event);
-//     });
-
-//     function sendMessage() {
-//         const messageInput = document.getElementById('message-input');
-//         const message = messageInput.value;
-//         socket.send(message);
-//         messageInput.value = '';
-//     }
-
-//     // Fetch and display news
-//     fetch('/getNews')
-//         .then(response => response.json())
-//         .then(news => {
-//             const newsList = document.getElementById('news-list');
-//             news.forEach(article => {
-//                 newsList.innerHTML += `<li>${article.title}</li>`;
-//             });
-//         });
-// });
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Fetch and display news
+ 
     fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-11-28&sortBy=publishedAt&apiKey=19341cab51894187aa10e39ad80a55d7')
         .then(response => response.json())
         .then(data => {
